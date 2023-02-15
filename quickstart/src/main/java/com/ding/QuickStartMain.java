983d0ccbdb4ae1130e999860b2af23dcb69d9e85
dingxitao <1131696215@qq.com>
2023-02-15 12:24:48 +0800

    初始化

---
+++ quickstart/src/main/java/com/ding/QuickStartMain.java
@@ -0,0 +1,11 @@
+package com.ding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartMain {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartMain.class, args);
+    }
+}
