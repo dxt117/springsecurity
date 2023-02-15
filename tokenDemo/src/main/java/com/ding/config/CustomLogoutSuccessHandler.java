a114d74d8a14e41b0396891610d9146224971982
dingxitao <1131696215@qq.com>
2023-02-15 16:02:17 +0800

    注销登陆成功处理

--- tokenDemo/src/main/java/com/ding/config/CustomLogoutSuccessHandler.java
+++ tokenDemo/src/main/java/com/ding/config/CustomLogoutSuccessHandler.java
@@ -2,9 +2,8 @@ package com.ding.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,15 +12,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 自定义认证成功后处理
+ * 自定义注销成功后处理
  *
  */
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         Map<String, Object> result = new HashMap<>();
-        result.put("msg", "登录成功");
+        result.put("msg", "注销成功");
         result.put("status", 200);
         result.put("authentication", authentication);
         response.setContentType("application/json;charset=UTF-8");
--- tokenDemo/src/main/java/com/ding/config/WebSecurityConfiguration.java
+++ tokenDemo/src/main/java/com/ding/config/WebSecurityConfiguration.java
@@ -6,15 +6,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.ForwardAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
@@ -43,8 +52,23 @@ public class WebSecurityConfiguration {
 //                .successForwardUrl("/hello/hello") //(适用于传统项目前后端不分离) 指定认证成功后 forward 跳转的路径   不会根据上一次保存的请求进行跳转
 //                .defaultSuccessUrl("/hello/hello") //(适用于传统项目前后端不分离) 认证成功后 redirect 跳转路径  会根据上一次保存的请求进行跳转
                 .successHandler(new CustomAuthenticationSuccessHandler()) //(前后端分离项目)认证成功时的处理
+//                .failureForwardUrl("") //认证失败后 forward 跳转的路径
+//                .failureUrl("") //认证失败后 redirect 跳转路径
+                .failureHandler(new CustomAuthenticationFailureHandler())//适用于前后端分离项目
+                .and()
+                .logout()
+                .logoutUrl("/logout") //指定注销登陆url   默认请求方式必须：GET
+//                .logoutRequestMatcher(new OrRequestMatcher(  //指定多个注销请求
+//                        new AntPathRequestMatcher("/aa","GET"),
+//                        new AntPathRequestMatcher("/bb","POST")
+//                ))
+                .logoutSuccessHandler(new CustomLogoutSuccessHandler()) //适用于前后端分离项目 注销登录后处理
+                .invalidateHttpSession(true)   //默认   session会话失效
+                .clearAuthentication(true)  //默认   清除认证标记
+//                .logoutSuccessUrl("login.html") // 注销后跳转路径
                 .and()
                 .csrf().disable(); //禁止跨站请求保护
+
 //        http.authorizeRequests().antMatchers("/public/hello").permitAll().anyRequest()
 //                .hasRole("USER").and()
 //                // Possibly more configuration ...
