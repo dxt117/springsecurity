3c8b44974bf20b12d1a97f8b893cefb4db19e171
dingxitao <1131696215@qq.com>
2023-02-19 15:07:06 +0800

    security  lambda 表达式方式配置

--- userDetailsDemo/src/main/java/com/ding/controller/LoginController.java
+++ userDetailsDemo/src/main/java/com/ding/controller/LoginController.java
@@ -1,14 +1,42 @@
 package com.ding.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
-public class LoginController {
-
+public class LoginController
+{
 
     @GetMapping("/login.html")
-    public String login() {
+    @ApiOperation("跳转到登录页")
+    public String login()
+    {
         return "login";
+    }
+
+    @PostMapping("/doLogin")
+    @ResponseBody
+    @ApiOperation("登录")
+    public String doLogin(
+            @ApiParam(required = true, value = "username", example = "root")
+            @RequestParam(value = "username")
+                    String username,
+            @ApiParam(required = true, value = "password", example = "123")
+            @RequestParam(value = "password")
+                    String password
+    )
+    {
+        return null;
+    }
+
+    @GetMapping("/logout")
+    @ResponseBody
+    @ApiOperation("注销登录")
+    public String logout()
+    {
+        return null;
     }
 }
